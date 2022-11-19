2e3a13fc2f8836d126ffec92d1ca8b1cc763fcd4
rawwr <akshayace0302@gmail.com>
2022-11-19 23:26:39 +0530

    handling moderation

---
+++ moderation/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", async (req, res, next) => {
+  const { type, data } = req.data;
+
+  if (type === "CommentCreated") {
+    //checking if orange is present in
+    const status = data.content.include("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4003, () => {
+  console.log("Listening on 4003");
+});
