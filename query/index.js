6c895bf6315145e9cf504b21ad10cafd71b1782d
rawwr <akshayace0302@gmail.com>
2022-11-20 02:22:34 +0530

    comment updated thru eventbus

--- query/index.js
+++ query/index.js
@@ -35,14 +35,26 @@ app.post("/events", (req, res) => {
   }
 
   if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
 
-  console.log(posts);
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
 
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
+  //   console.log(posts);
+  //   console.log(data);
   res.send({});
 });
 
